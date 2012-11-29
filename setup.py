3d84f8a3b6c4e8d0be921d4127ab6fc4a52200cd
Sander van de Graaf <mail@svdgraaf.nl>
2012-11-29 16:00:35 +0100

    initial setup settings

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='django-notificare',
+    version='0.1.0',
+    description='Notificare Python client',
+    #long_description = read('README.rst'),
+    author='Sander van de Graaf',
+    url='https://github.com/svdgraaf/django-notificare',
+    install_requires=('requests',),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
