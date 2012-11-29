d7969a77d5ccb96f1307de62bee635fe9c1b9d95
Sander van de Graaf <mail@svdgraaf.nl>
2012-11-29 18:51:32 +0100

    fixed sending of wrong formatted date to the api, cleanup

--- django_notificare/__init__.py
+++ django_notificare/__init__.py
@@ -5,6 +5,7 @@ import requests
 def send(message, full_message=None, targets=[]):
     service_token = settings.NOTIFICARE_SERVICE_TOKEN
     user_token = settings.NOTIFICARE_USER_TOKEN
+    import json
 
     url = 'https://apps.notifica.re/hooks/webhook/{service}?token={user}'.format(service=service_token, user=user_token)
     data = {
@@ -12,14 +13,16 @@ def send(message, full_message=None, targets=[]):
         'fullMessage': full_message,
         'targets': targets
     }
-    r = requests.post(url, data)
+    print data
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
     return r
 
 
 # action for opening a url (with schema)
-def url(url, message=None, full_message=None, keyboard=False):
+def url(url, message=None, full_message=None, keyboard=False, action='Open web page'):
     target = {
-        'action': 'open web page',
+        'action': action,
         'url': url,
         'type': 'url',
         'message': keyboard,
@@ -28,9 +31,9 @@ def url(url, message=None, full_message=None, keyboard=False):
 
 
 # callback url, defaults keyboard enabled
-def callback(url, message=None, full_message=None, keyboard=True):
+def callback(url, message=None, full_message=None, keyboard=True, action='Do it'):
     target = {
-        'action': 'reply by message',
+        'action': action,
         'url': url,
         'type': 'callback',
         'message': keyboard,
@@ -40,19 +43,33 @@ def callback(url, message=None, full_message=None, keyboard=True):
 
 # call the given number
 def call(number=None, message=None, full_message=None, keyboard=False):
-    return url('mailto:{number}'.format(number=number), message, full_message, keyboard)
+    return url('tel://{number}'.format(number=number),
+                message,
+                full_message,
+                keyboard,
+                action='Call {number}'.format(number=number)
+            )
 
 
 # send an action to start a mail to the given address
 def email(address, message=None, full_message=None, keyboard=False):
-    return url('mailto:{address}'.format(address=address), message, full_message, keyboard)
+    return url('mailto:{address}'.format(address=address),
+                message,
+                full_message,
+                keyboard,
+                action='Email {address}'.format(address=address)
+            )
 
 
 # open given url in safari
 def browse(url, message=None, full_message=None):
-    return url('safari://{url}'.format(url=url), message, full_message, False)
+    return url('safari://{url}'.format(url=url),
+                message,
+                full_message,
+                False
+            )
 
 
 # reply to something, and send the details to the given url (enables keyboard)
 def reply(post_to, message=None, full_message=None, keyboard=True):
-    return callback(post_to, message, full_message, keyboard)
+    return callback(post_to, message, full_message, keyboard, message='Reply')
